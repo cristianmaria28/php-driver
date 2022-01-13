5a73782ab6f9ee4d9e43bce7a173d79382d1fba2
Maria Cristian <maria.cristian28@gmail.com>
2022-01-13 22:15:36 +0200

    Update install.sh

--- ext/install.sh
+++ ext/install.sh
@@ -25,7 +25,7 @@ builddir=$(cd build; pwd)
 echo "Compiling cpp-driver..."
 mkdir cpp-driver
 pushd cpp-driver
-cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$builddir -DCASS_BUILD_STATIC=ON \
+cmake -DCMAKE_CXX_FLAGS="-fPIC -Wno-error=format-truncation" -DCMAKE_INSTALL_PREFIX:PATH=$builddir -DCASS_BUILD_STATIC=ON \
   -DCASS_BUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCASS_USE_ZLIB=ON \
   -DCMAKE_INSTALL_LIBDIR:PATH=lib $basedir/../lib/cpp-driver/
 make CXXFLAGS="-Wno-error=format-truncation"
